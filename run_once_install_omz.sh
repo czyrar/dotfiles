cc2121004928d32d51a9ea683760f86e9b7b0053
C. Roldán Piñero <roldanpineroc@gmail.com>
2025-09-11 15:16:38 +0200

    added p10k

--- run_once_install_omz.sh
+++ run_once_install_omz.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc --skip-chsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
