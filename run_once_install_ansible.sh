dba7648bab81c2ce2f839828e4def53712ea17c8
C. Roldán Piñero <roldanpineroc@disroot.org>
2025-12-19 00:53:11 +0100

    major changes

--- run_once_install_ansible.sh
+++ run_once_install_ansible.sh
@@ -1,34 +1,5 @@
 #!/bin/bash
 
-install_on_fedora() {
-  sudo dnf install -y ansible
-  ansible-playbook ~/.bootstrap/fedora.yml --ask-become-pass
-}
-
-install_on_arch() {
-  sudo pacman -S ansible --noconfirm
-  ansible-galaxy install mnussbaum.ansible-yay
-  ansible-playbook ~/.bootstrap/arch.yml --ask-become-pass
-}
-
-. /usr/lib/os-release
-if [ -z "$ID_LIKE" ]; then
-  DISTRO=$ID
-else
-  DISTRO=$ID_LIKE
-fi
-
-case "${DISTRO}" in
-  fedora)
-    install_on_fedora
-    ;;
-  arch)
-    install_on_arch
-    ;;
-  *)
-    echo "Unsupported linux distro: ${ID}"
-    exit 1
-    ;;
-esac
-
-ansible-playbook ~/.bootstrap/common.yml --ask-become-pass
+sudo pacman -S ansible --noconfirm
+ansible-galaxy install mnussbaum.ansible-yay
+ansible-playbook ~/.bootstrap/arch.yml --ask-become-pass
--- run_once_install_juliaup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -fsSL https://install.julialang.org | sh
-source "${HOME}/.profile"
