c23fcaee4395c0b6955d1d8df55ac03f70713357
C. Roldán Piñero <roldanpineroc@disroot.org>
2025-12-25 03:34:50 +0100

    please

--- run_once_install_omz.sh
+++ run_once_install_omz.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -d "$HOME/.oh-my-zsh" ]; then
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc --skip-chsh
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
