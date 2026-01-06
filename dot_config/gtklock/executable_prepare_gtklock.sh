0e9871bef1cd45aac1b8da74f45d67c6a26b2b7b
C. Roldán Piñero <roldanpineroc@disroot.org>
2026-01-06 21:23:41 +0100

    sway yay

--- dot_config/gtklock/executable_prepare_gtklock.sh
+++ dot_config/gtklock/executable_prepare_gtklock.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-swaylock_config="$HOME/.config/swaylock/config"
-swaylock_config_template="$HOME/.config/swaylock/config_template"
-
-# Base part
-cat "$swaylock_config_template" > "$swaylock_config"
+style_file="$HOME/.config/gtklock/style.css"
+[ -f "$style_file" ] && rm "$style_file" || touch "$style_file"
 
 # Background
-cache_dict="$HOME/.config/swaylock/.cache"
+cache_dict="$HOME/.config/gtklock/.cache"
 [ -d "$cache_dict" ] || mkdir -p "$cache_dict"
 query=$(swww query | cut -d' ' -f2,9)
 echo "$query" | while IFS= read -r wallpaper_info; do
@@ -15,5 +12,15 @@ echo "$query" | while IFS= read -r wallpaper_info; do
   wallpaper=$(printf "%s" "$wallpaper_info" | cut -d' ' -f2)
   lockpaper="$cache_dict/$(basename $wallpaper)"
   [ -f "$lockpaper" ] || magick "$wallpaper" -filter Gaussian -resize 2.5% -define filter:sigma=2.5 -resize 4000% "$lockpaper"
-  echo "image=$output$lockpaper" >> "$swaylock_config"
+  echo "window#${output%?} {" >> "$style_file"
+  echo "  background-image: url(\"$lockpaper\");" >> "$style_file"
+  echo "}" >> "$style_file"
 done
+
+cat << EOF >> "$style_file"
+#window-box {
+  padding: 64px;
+  border: 1px solid #BB9AF7;
+  background-color: #1A1B26;
+}
+EOF
